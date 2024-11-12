2131bffc504c0b75df535e80bc080954e446e7eb
Fathurrahman <10190031@bsi.ac.id>
2024-11-12 20:37:16 +0700

    Add emojis

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,11 +12,10 @@ export interface Platform {
 export interface Game {
   id: number;
   name: string;
-  released?: string;
   background_image: string;
-  rating?: number;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
+  rating_top: number;
 }
 
 const useGames = (gameQuery: GameQuery) =>
